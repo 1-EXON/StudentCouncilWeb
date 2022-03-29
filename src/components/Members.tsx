ab076e5b90232370c76774a268aa9d09e2281b4d
시혁 박 <exon.indiv@gmail.com>
2022-03-29 22:13:59 +0900

    Edit: dark mode

--- src/components/Members.tsx
+++ src/components/Members.tsx
@@ -5,7 +5,7 @@ type MemberProps = {
 
 export default function Nav({name, position}: MemberProps) {
     return (
-        <div id="card" className="bg-white-theme px-4 py-2.5 m-2 rounded dark:bg-gray-800">
+        <div id="card" className="bg-gray-800 px-4 py-2.5 m-2 rounded">
             <h1 className="text-white-theme text-2xl font-bold">{name}</h1>
             <p className="text-white-theme text-l pt-1">{position}</p>
         </div>
