926c9127af3eab82aad11aee5968f5e4023a8a49
시혁 박 <exon.indiv@gmail.com>
2022-04-04 22:04:24 +0900

    Edit: font-size

--- src/components/Members.tsx
+++ src/components/Members.tsx
@@ -6,8 +6,8 @@ type MemberProps = {
 export default function Nav({name, position}: MemberProps) {
     return (
         <div id="card" className="bg-gray-800 px-4 py-2.5 m-2 rounded">
-            <h1 className="text-white-theme text-2xl font-bold">{name}</h1>
-            <p className="text-white-theme text-l pt-1">{position}</p>
+            <h1 className="text-white-theme text-xl font-bold">{name}</h1>
+            <p className="text-white-theme text-base pt-1">{position}</p>
         </div>
     )
 }
