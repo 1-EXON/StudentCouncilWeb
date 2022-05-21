dc5ae04156e8e96d1dc4b239c17d443c71ad54d8
시혁 박 <exon.indiv@gmail.com>
2022-05-21 20:29:33 +0900

    Update: clubs

--- src/components/ClubCard.tsx
+++ src/components/ClubCard.tsx
@@ -5,9 +5,9 @@ type ClubProps = {
 
 export default function ClubCard({name, description}: ClubProps) {
     return (
-        <div id="card" className="text-center bg-gray-800 w-72 px-2 sm:px-4 py-2.5 m-2 rounded">
-            <h1 className="text-white-theme text-3xl font-bold">{name}</h1>
-            <p className="text-white-theme text-l pt-1">{description}</p>
+        <div id="card" className="text-center bg-gray-800 w-60 px-2 sm:px-4 py-2.5 m-2 rounded">
+            <h1 className="text-white-theme w-30 text-2xl font-bold text-ellipsis overflow: hidden;">{name}</h1>
+            <p className="text-white-theme w-60 text-l pt-1 text-ellipsis overflow: hidden;">{description}</p>
             <div className="inline-flex">
                 <button className="rounded bg-transparent font-semibold border-red-theme mt-3 mx-1 py-2 px-6 border text-red-theme hover:text-white hover:bg-red-theme">신청하기</button>
             </div>
--- src/pages/Club.tsx
+++ src/pages/Club.tsx
@@ -2,7 +2,13 @@ import Banner from '../components/Banner'
 import ClubCard from '../components/ClubCard'
 
 export default function Club() {
-    const clubs: [string, string][] = [['LOOKS', '댄스동아리'],['LOOKS', '댄스동아리'],['LOOKS', '댄스동아리'],]
+    const c = require('../resources/clubs.json')
+    const clubs: [string, string][] = []
+    c.forEach((element: { name: string, sort: string }) => {
+        clubs.push([element.name, element.sort])
+    })
+    console.log(c)
+    
     return (
         <div>
             <Banner />
