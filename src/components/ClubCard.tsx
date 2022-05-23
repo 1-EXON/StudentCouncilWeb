3dea1780eaca8fb2425bdd5244b62fd3709573ad
EXON <exon.indiv@gmail.com>
2022-05-23 04:07:34 +0000

    Update: club card

--- src/components/ClubCard.tsx
+++ src/components/ClubCard.tsx
@@ -6,11 +6,8 @@ type ClubProps = {
 export default function ClubCard({name, description}: ClubProps) {
     return (
         <div id="card" className="text-center bg-gray-800 w-60 px-2 sm:px-4 py-2.5 m-2 rounded">
-            <h1 className="text-white-theme w-30 text-2xl font-bold text-ellipsis overflow: hidden;">{name}</h1>
-            <p className="text-white-theme w-60 text-l pt-1 text-ellipsis overflow: hidden;">{description}</p>
-            <div className="inline-flex">
-                <button className="rounded bg-transparent font-semibold border-red-theme mt-3 mx-1 py-2 px-6 border text-red-theme hover:text-white hover:bg-red-theme">신청하기</button>
-            </div>
+            <h1 className="text-white-theme w-30 text-2xl font-bold">{name}</h1>
+            <p className="text-white-theme w-60 text-l pt-1">{description}</p>
         </div>
         
     )
